6530dde0b90023ceed00aca11b5ae439c8820749
juuh22 <juliasouzarlk22@gmail.com>
2025-05-14 15:51:55 -0300

    Add files via upload

--- desconto.py
+++ desconto.py
@@ -3,4 +3,5 @@ preço = float(input('preço do produto: '))
 porcentagem = float(input('porcentagem com desconto: '))
 valor_desconto = preço * (porcentagem/ 100)
 valor_novo = preço - valor_desconto
-print(valor_novo)
+
+print(f'O {produto} com {porcentagem}% de desconto custará: R$ {valor_novo}')
