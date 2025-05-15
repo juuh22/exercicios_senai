cd2e329c377b92000e041b00c06ec5bfdd855efc
juuh22 <juliasouzarlk22@gmail.com>
2025-05-15 15:35:33 -0300

    Add files via upload

---
+++ dia.py
@@ -0,0 +1,17 @@
+numero = input('Digite um numero:')
+if numero == '1':
+    print('Domingo')
+elif numero == '2':
+    print('Segunda feira')
+elif numero == '3':
+    print('Terça feira')
+elif numero == '4':
+    print('Quarta feira')
+elif numero == '5':
+    print('Quinta feira')
+elif numero == '6':
+    print('Sexta feira')
+elif numero == '7':
+    print('Sabado')
+else:
+    print('Numero errado')                        
