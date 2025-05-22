2bd7bb2b871e838c9d5e401a3b96c796a0d104b8
juuh22 <juliasouzarlk22@gmail.com>
2025-05-22 14:38:20 -0300

    Add files via upload

---
+++ carro.py
@@ -0,0 +1,7 @@
+n_carro = input('Digite o nome do carro: ')
+v_carro = input('Digite o valor do carro: ')
+c_carro = input('Digite o consumo KM por litro: ')
+
+print('Carro:', n_carro)
+print('Valor:', v_carro)
+print('consumo por litro:', c_carro)
